ff4d32ec68b4a244293b0b91420fb78ac050bb12
Aria Fong <aria.nimue@gmail.com>
2018-12-25 22:52:16 +0800

    feat: added routes and extra components

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,8 +1,13 @@
-import React, { Component } from "react";
-import { Box, Flex, Link, Text, Heading } from "rebass";
+import React, { Component, Fragment } from "react";
+import { Box, Flex, Text, Heading } from "rebass";
 import "./App.css";
 import { ThemeProvider } from "styled-components";
 import { Folder, Send, Info } from "react-feather";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link as RouteLink
+} from "react-router-dom";
 
 const darkslateblue = "#483d8b";
 const darkcyanblue = "#080d16";
@@ -31,70 +36,111 @@ const theme = {
 class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <Flex
-          flexWrap="wrap"
-          className="main"
-          color={pinkred}
-          justifyContent="center"
-        >
-          <Box width={[1, 1 / 3, 4 / 7]} py={3}>
-            <Link className="codemoe" href="/" color="white">
-              <Text className="brand" fontSize={3}>
-                /0/!hack
-              </Text>
-            </Link>
-          </Box>
-          <Box width={[1 / 3, 2 / 21]} py={3}>
-            <Link className="codemoe" href="/about" color="inherit" py={[0, 1]}>
-              <Info />
-              <Text>about</Text>
-            </Link>
-          </Box>
-          <Box width={[1 / 3, 2 / 21]} py={3}>
-            <Link
-              className="codemoe"
-              href="/projects"
-              color="inherit"
-              py={[0, 1]}
-            >
-              <Folder />
-              <Text>projects</Text>
-            </Link>
-          </Box>
-          <Box width={[1 / 3, 2 / 21]} py={3}>
-            <Link
-              className="codemoe"
-              href="/contact"
-              color="inherit"
-              py={[0, 1]}
-            >
-              <Send />
-              <Text>contact</Text>
-            </Link>
-          </Box>
-          <Box width={[1, 4 / 7]}>
-            <Heading className="codehack" fontSize={5}>
-              cd /0/!hack
-            </Heading>
-            <Text className="coderesponse">bash: !hack: event not found</Text>
-          </Box>
-          <Box width={[1, 1 / 2, 2 / 7]}>
-            <Text fontSize={[3, 5]} color="white">
-              tech nomad
-            </Text>
-            <Text>devops: ubuntu 18.04 git</Text>
-            <Text fontSize={[3, 5]} color="white">
-              artful developer
-            </Text>
-            <Text>webdev: node react laravel bootstrap material</Text>
-            <Text fontSize={[3, 5]} color="white">
-              critical designer
-            </Text>
-            <Text>ui/ux: photoshop illustrator xd</Text>
-          </Box>
-        </Flex>
-      </ThemeProvider>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <Flex
+            flexWrap="wrap"
+            className="main"
+            color={pinkred}
+            justifyContent="center"
+          >
+            <Box width={[1, 1 / 3, 4 / 7]} py={3}>
+              <RouteLink to="/" className="codemoe">
+                <Text className="brand" fontSize={3}>
+                  /0/!hack
+                </Text>
+              </RouteLink>
+            </Box>
+            <Box width={[1 / 3, 2 / 21]} py={3}>
+              <RouteLink to="/about" className="codemoe">
+                <Info />
+                <Text>about</Text>
+              </RouteLink>
+            </Box>
+            <Box width={[1 / 3, 2 / 21]} py={3}>
+              <RouteLink to="/projects" className="codemoe">
+                <Folder />
+                <Text>projects</Text>
+              </RouteLink>
+            </Box>
+            <Box width={[1 / 3, 2 / 21]} py={3}>
+              <RouteLink to="/contact" className="codemoe">
+                <Send />
+                <Text>contact</Text>
+              </RouteLink>
+            </Box>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+          </Flex>
+        </ThemeProvider>
+      </Router>
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Box width={[1, 4 / 7]}>
+          <Heading className="codehack" fontSize={5}>
+            cd /0/!hack
+          </Heading>
+          <Text className="coderesponse">bash: !hack: event not found</Text>
+        </Box>
+        <Box width={[1, 1 / 2, 2 / 7]}>
+          <Text fontSize={[3, 5]} color="white">
+            tech nomad
+          </Text>
+          <Text>devops project_management</Text>
+          <Text fontSize={[3, 5]} color="white">
+            artful developer
+          </Text>
+          <Text>webdev node react laravel</Text>
+          <Text fontSize={[3, 5]} color="white">
+            critical designer
+          </Text>
+          <Text>ui/ux adobe product_design</Text>
+        </Box>
+      </Fragment>
+    );
+  }
+}
+
+class About extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Box width={1}>
+          <Heading color="white">About</Heading>
+        </Box>
+      </Fragment>
+    );
+  }
+}
+
+class Projects extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Box width={1}>
+          <Heading color="white">Projects</Heading>
+        </Box>
+      </Fragment>
+    );
+  }
+}
+
+class Contact extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Box width={1}>
+          <Heading color="white">Contact</Heading>
+        </Box>
+      </Fragment>
     );
   }
 }
