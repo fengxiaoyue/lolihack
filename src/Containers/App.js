50695aaaf281f305db4ed339ef1f831cf8979866
Aria Fong <aria.nimue@gmail.com>
2019-03-08 15:26:05 +0800

    feat: fixed layout, updated dependencies

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import { Box, Flex, Text, Heading } from "rebass";
 import { Folder, Send, Info } from "react-feather";
+import { ThemeProvider } from "styled-components";
 import {
   BrowserRouter as Router,
   Route,
@@ -12,72 +13,87 @@ const offwhite = "#FAFAFA";
 const fraudulent = "#0D324D";
 const election = "#7F5A83";
 
+const theme = {
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
+  fonts: {
+    sans: "system-ui, sans-serif",
+    mono: "Share Tech Mono, monospace"
+  }
+};
+
 class App extends Component {
   render() {
     return (
       <Router>
-        <Flex
-          flexWrap="wrap"
-          bg={abyss}
-          color={fraudulent}
-          justifyContent="center"
-          css={{
-            height: "100vh",
-            width: "100vw",
-            a: { textDecoration: "none" }
-          }}
-        >
-          <Flex width={1}>
-            <Box width={2 / 3} p={4}>
-              <RouteLink to="/">
-                <Text fontSize={3} color={offwhite}>
-                  /0/!hack
-                </Text>
-              </RouteLink>
-            </Box>
-            <Flex width={1 / 3} p={4} justifyContent="flex-end">
-              <Box px={4}>
-                <RouteLink to="/about">
-                  <Flex color={offwhite} alignItems="center">
-                    <Box pr={2}>
-                      <Info size={20} />
-                    </Box>
-                    <Text fontSize={16}>about</Text>
-                  </Flex>
+        <ThemeProvider theme={theme}>
+          <Flex
+            flexWrap="wrap"
+            bg={abyss}
+            color={fraudulent}
+            justifyContent="center"
+            css={{
+              height: "100vh",
+              width: "100vw",
+              a: { textDecoration: "none" }
+            }}
+          >
+            <Flex width={1} px={7} py={4}>
+              <Box width={2 / 3}>
+                <RouteLink to="/">
+                  <Text fontSize={3} color={offwhite}>
+                    /0/!hack
+                  </Text>
                 </RouteLink>
               </Box>
-              <Box px={4}>
-                <RouteLink to="/projects">
-                  <Flex color={offwhite} alignItems="center">
-                    <Box pr={2}>
-                      <Folder size={20} />
-                    </Box>
-                    <Text fontSize={16}>projects</Text>
-                  </Flex>
-                </RouteLink>
-              </Box>
-              <Box px={4}>
-                <RouteLink to="/contact">
-                  <Flex color={offwhite} alignItems="center">
-                    <Box pr={2}>
-                      <Send size={20} />
-                    </Box>
-                    <Text fontSize={16}>contact</Text>
-                  </Flex>
-                </RouteLink>
-              </Box>
+              <Flex width={1 / 3} justifyContent="flex-end">
+                <Box px={4}>
+                  <RouteLink to="/about">
+                    <Flex color={offwhite} alignItems="center">
+                      <Box pr={2}>
+                        <Info size={20} />
+                      </Box>
+                      <Text fontSize={16}>about</Text>
+                    </Flex>
+                  </RouteLink>
+                </Box>
+                <Box px={4}>
+                  <RouteLink to="/projects">
+                    <Flex color={offwhite} alignItems="center">
+                      <Box pr={2}>
+                        <Folder size={20} />
+                      </Box>
+                      <Text fontSize={16}>projects</Text>
+                    </Flex>
+                  </RouteLink>
+                </Box>
+                <Box px={4}>
+                  <RouteLink to="/contact">
+                    <Flex color={offwhite} alignItems="center">
+                      <Box pr={2}>
+                        <Send size={20} />
+                      </Box>
+                      <Text fontSize={16}>contact</Text>
+                    </Flex>
+                  </RouteLink>
+                </Box>
+              </Flex>
+            </Flex>
+            <Flex width={1} px={7}>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/projects" component={Projects} />
+              <Route path="/contact" component={Contact} />
+            </Flex>
+            <Flex
+              width={1}
+              px={7}
+              alignItems="center"
+              justifyContent="flex-end"
+            >
+              <Text color={offwhite}>Hello</Text>
             </Flex>
           </Flex>
-          <Flex width={1}>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/contact" component={Contact} />
-          </Flex>
-          <Flex width={1} alignItems="center" justifyContent="flex-end">
-            <Text color={offwhite}>Hello</Text>
-          </Flex>
-        </Flex>
+        </ThemeProvider>
       </Router>
     );
   }
@@ -87,38 +103,42 @@ class Home extends Component {
   render() {
     return (
       <>
-        <Box width={2 / 3} p={4}>
-          <Heading className="codehack" fontSize={5}>
-            cd /0/!hack
-          </Heading>
-          <Text className="coderesponse">bash: !hack: event not found</Text>
-        </Box>
-        <Box width={1 / 3} p={4}>
-          <Box width={1}>
-            <Text fontSize={[3, 5]} color={offwhite}>
-              tech nomad
-            </Text>
-            <Text bg={fraudulent} color={election}>
-              software development & IT ops
-            </Text>
+        <Flex width={1 / 3} alignItems="center" justifyContent="center">
+          <Box>
+            <Box width={1} pb={2}>
+              <Text fontSize={36} color={offwhite} pb={2}>
+                tech nomad
+              </Text>
+              <Text bg={fraudulent} color={election}>
+                software development & IT ops
+              </Text>
+            </Box>
+            <Box width={1} pb={2}>
+              <Text fontSize={36} color={offwhite} pb={2}>
+                artful developer
+              </Text>
+              <Text bg={fraudulent} color={election}>
+                web & mobile development
+              </Text>
+            </Box>
+            <Box width={1} pb={2}>
+              <Text fontSize={36} color={offwhite} pb={2}>
+                critical designer
+              </Text>
+              <Text bg={fraudulent} color={election}>
+                user interface & user experience
+              </Text>
+            </Box>
           </Box>
-          <Box width={1}>
-            <Text fontSize={[3, 5]} color={offwhite}>
-              artful developer
-            </Text>
-            <Text bg={fraudulent} color={election}>
-              web & mobile development
-            </Text>
+        </Flex>
+        <Flex width={2 / 3} alignItems="center" justifyContent="center">
+          <Box>
+            <Heading className="codehack" fontSize={48}>
+              cd /0/!hack
+            </Heading>
+            <Text>bash: !hack: event not found</Text>
           </Box>
-          <Box width={1}>
-            <Text fontSize={[3, 5]} color={offwhite}>
-              critical designer
-            </Text>
-            <Text bg={fraudulent} color={election}>
-              user interface & user experience
-            </Text>
-          </Box>
-        </Box>
+        </Flex>
       </>
     );
   }
